e33527e184057cf660c7164e07afc3d72d722232
denisyen <denisyenrc@gmail.com>
2021-04-21 17:44:14 -0500

    Se agrego el color azul al gráfico

---
+++ Tema1.R
@@ -0,0 +1,25 @@
+# OULTIERS CONSULTING.SA.C
+# AUTOR: DENIS RODRIGUEZ CORTEGANA
+
+
+# Ejemplo para GitHub -----------------------------------------------------
+
+data <- cars
+head(cars)
+
+# se raliza una auditoria de datos 
+library(funModeling)
+str(cars)
+df_status(cars)
+
+
+# Realizamos un resumen de los datos
+summary(data)
+
+# Realizamos una medida de correlación
+cor(cars)
+
+# Gráfico
+library(ggplot2)
+ggplot(cars, aes(speed, dist))+
+  geom_point(colour = "blue")
