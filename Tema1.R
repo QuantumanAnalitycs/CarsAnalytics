54a28795e4d7c39a98e5ea29efedb58504231419
denisyen <denisyenrc@gmail.com>
2021-04-23 19:56:55 -0500

    Se agrego algo al titulo

--- Tema1.R
+++ Tema1.R
@@ -28,5 +28,5 @@ ggplot(cars, aes(speed, dist))+
 
 hist(cars)
 hist(cars$dist, breaks = 7, col = "Blue",
-     main = "Histograma de frequencias",xlab = "Divisiones",
+     main = "Histograma de frequencias de la velocidad",xlab = "Divisiones",
      ylab = "Frecuencia")
